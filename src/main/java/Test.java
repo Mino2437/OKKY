e14dfdca58cbc351d5a7ac4b5b95137722fed59e
Mino2437 <alsgh113344@gmail.com>
2024-10-24 22:55:21 +0900

    단계 1 예외처리마무리,  단계2 시작

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,89 +5,103 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Test
-{
+public class Test {
     public static void main(String[] args) {
 
         // 명령어 final로 키워드 상수로 정적배열에 넣기
         final String[] cmdArr = {
                 "작성", "조회", "수정", "삭제", "quit", "종료", "목록"
         };
+
+
+//        String s = " 1번 ";
+//        String b = s.trim();
+        Scanner sc = new Scanner(System.in);
+
+//        String[] ar = b.split("");
+//        String[] cmdSplit = sc.next().split("");
+//
+//        System.out.println(ar.length);
+//
+//        if (ar.length < 4) {
+//            System.out.println("여기서만 출력");
+//
+//            System.out.println(ar[0]);
+//            System.out.println(ar[1]);
+//            System.out.println(ar[2]);
+//
+//        } else System.out.println("예외처리");
+
+
         Board board = new Board();
+        Board board2 = new Board();
         String userCmd;
 
+
         String str = "명령어 > ";
-        HashMap<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
-        int i=1;
-        int j=1;
+//        HashMap<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
+
+//        HashMap<Integer, ArrayList<Board>> map2 = new HashMap<Integer, ArrayList<Board>>();
+//        Board 객체 타입으로 가능한지 확인중
+
+        // 여기에 재귀함수로 계속 호출하게 메인메서드 깔끔하게 정리, boardStart();
+
 
         while (true) {
 
+            // 메소드로 변환 예정
+            System.out.print("사용 가능 명령어 > ");
+
+            // 명령어 조회, 잘못된 입력했을때 ?를 입력하면 보여주게끔
+            System.out.println(Arrays.toString(cmdArr));
             System.out.print(str);
             userCmd = inputCmd();
             // 메모리 누수( obsolete reference ) 생길예정, 계속 확인하기
 
             if (userCmd.equals(cmdArr[0])) { // == "작성" Create
 
-                board.boardCreate();
-                System.out.println(board.getTitle() + "  " + board.getContent());
 
-                System.out.print("겟타이틀");
-                System.out.println(board.getTitle());
-
-                ArrayList<String> boardList= new ArrayList<String>(); // 어레이리스트를
-                boardList.add(board.getTitle());
-                boardList.add(board.getContent());
-                System.out.println("보드리스트 "+ boardList);
-
-                map.put(i,boardList);
-                i++;
-                System.out.println("맵밸류" + map.values());
-                System.out.println("키셋" + map.keySet());
-                System.out.println("맵사이즈" + map.size());
+                // board클래스 게시글 작성 메소드 호출
+                board.boardCreate(); // 게시판1(커뮤니티)
+//                board2.boardCreate(); // 게시판2(Q&A) 작성, 호출
 
                 // 메소드 인자가 두개일때, 세개일때 그나머지일때 예외처리
-                // board클래스 게시글 작성 메소드 호출
                 // 여기서도 잘못된 명령어 예외처리 및 사용 가능한 명령어 출력 처리 어떻게 할지 생각
 
             } else if (userCmd.equals(cmdArr[1])) { // == "조회" Read
 
-
                 // board클래스 게시글 조회 메소드 호출
+                board.boardRead();
                 // 여기서도 잘못된 명령어 예외처리 및 사용 가능한 명령어 출력 처리 어떻게 할지 생각
-                break;
+
             } else if (userCmd.equals(cmdArr[2])) { // == "수정" Update
 
                 // board클래스 게시글 수정 메소드 호출
+                board.boardUpdate();
                 // 여기서도 잘못된 명령어 예외처리 및 사용 가능한 명령어 출력 처리 어떻게 할지 생각
-                break;
             } else if (userCmd.equals(cmdArr[3])) { // == "삭제" Delete
 
                 // board클래스 게시글 삭제 메소드 호출
+                board.boardDelete();
                 // 여기서도 잘못된 명령어 예외처리 및 사용 가능한 명령어 출력 처리 어떻게 할지 생각
-                break;
+
             } else if (userCmd.equals(cmdArr[4]) || userCmd.equals(cmdArr[5])) { // == "quit", "종료"
 
                 // board클래스 종료 메소드 호출
-
                 System.out.println("프로그램이 종료됩니다.");
                 break;
-
             } else if (userCmd.equals(cmdArr[6])) { // == "목록"
 
                 // board클래스 게시글 목록 메소드 호출
-                break;
+                board.boardCategory();
+
             } else {
                 // board클래스 잘못된 명령어 메소드 호출
-                System.out.println("잘못된 입력 입니다. 다시 입력 하세요.");
-//                Board board = new Board("게시글 제목", "게시글 내용"); // 제목 내용 멤버변수로
-//                Board board2 = new Board(1, "게시판 제목", "게시판 내용");
-//                board2.boardCreate();
+                System.out.println("존재하지 않는 명령어 입니다. 다시 입력 하세요.");
 
 
                 // 클래스내 메소드로 변경 예정
-                System.out.print("사용 가능 명령어 > ");
-                System.out.println(Arrays.toString(cmdArr)); // 명령어 조회, 잘못된 입력했을때 ?를 입력하면 보여주게끔
+
             }
 //              else if (
 //                    userCmd.equals("작성") || userCmd.equals("조회") || userCmd.equals("삭제") || userCmd.equals("수정")
@@ -95,7 +109,7 @@ public class Test
 //                switch (userCmd) {
 //                    case "작성": { // 명령어를 부모 클래스에 멤버변수로 상수로 둬서 부모.name이 같은지, 다만 클래스 외부에서 변경 불가
 
-                        // if userCmd, 작성 제목+내용, 번호+제목+내용
+            // if userCmd, 작성 제목+내용, 번호+제목+내용
 //                        Board board = new Board("게시글 제목", "게시글 내용"); // 제목 내용 멤버변수로
 ////                        Board board2 = new Board(1, "게시판 제목", "게시판 내용");
 //                        board.boardCreate();
@@ -114,6 +128,7 @@ public class Test
 
         }
     }
+
     public static String inputCmd() {
 
         String userCmd;
--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
+
 public class Board {
 
+    HashMap<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>(); // ArrList 인자로, # 1번
+//    HashMap<Integer, ArrayList<Board>> map = new HashMap<Integer, ArrayList<Board>>(); // # 2번
+    // Map, ArrList<객체>로 받음
 
     // 게시판 상태
     // 게시판 번호
@@ -14,12 +18,13 @@ public class Board {
     private String title;
     // 게시판 내용
     private String content;
+    private ArrayList<Board> boardList;
 
     public Board() {
 
     }
 
-    public Board(String title ,String content ) {
+    public Board(String title ,String content ) { // 제목과 내용으로 입력받아서
 
         this.title = title; // 게시판 제목
         this.content = content; // 게시판 내용
@@ -40,41 +45,175 @@ public class Board {
     // 하위클래스를 CRUD로 나눔
 
     // 행동, CRUD , 작성 Create, 조회 Read, 수정 Update, 삭제 Delete
+
+    // 0. Start 게시글 동작, 재귀호출
+    public void boardStart() {
+
+    };
+
     // 1. Create 게시글 작성
     public void boardCreate() {
+
         System.out.print("제목을 입력하세요 > ");
-        String tmp1 = inputCmd();
-        this.title = tmp1;
+        this.title = inputCmd();
 
         System.out.print("내용을 입력하세요 > ");
-        String tmp2 = inputCmd();
-        this.content = tmp2;
+        this.content = inputCmd();
         // 입력도 여기서 받게끔
 
-//        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
-//        ArrayList<String> boardList= new ArrayList<String>();
-//        boardList.add(title);
-//        boardList.add(content);
-//        map.put("1", boardList);
-//        System.out.println(map.values());
-//
-        Board board = new Board(tmp1, tmp2);
+        ArrayList<String> boardList = new ArrayList<String>(); // 어레이리스트를 인자로 map에 넣기위해 , # 1번
+//        ArrayList<Board> boardList = new ArrayList<Board>(); // 어레이리스트를 인자로 map에 넣기위해 , # 2번
+        // boardCreate에서의 값만 어레이리스트에 넣기 위해, 하나의 map value에 쌓이지 않기위해 여기서 사용
+
+        boardList.add(this.title); // # 1번
+        boardList.add(this.content); // # 2번
+
+//        boardList.add(new Board(this.title,this.content)); // # 2번
+        this.num++; // 작성 호출할때마다 게시글 번호 증가
+
+        map.put(this.num, boardList);
+        map.put(this.num, boardList);
+
+        System.out.println("밸류" + map.get(this.num));
+
+        System.out.println("맵밸류" + map.values());
+        System.out.println("키셋" + map.keySet());
+        System.out.println("맵사이즈" + map.size());
+
 
     }
+
     // 2. Read 게시글 조회
     public void boardRead() {
-        System.out.println(title + "을/를 조회중");
+
+        System.out.print("어떤 게시물을 조회할까요? ");
+        System.out.println("등록된 게시물 번호 : " + map.keySet());
+        System.out.println("입력 예시 : 1번 또는 1 ");
+//        System.out.println(numConv);
+
+//        String tmp = inputCmd();
+
+        String[] tmpSplit = inputCmd().split("번");
+
+        Integer key = Integer.parseInt(tmpSplit[0]);
+        // key값은 primitive 타입은 올수없고, Reference type인 Integer로 정수형을 사용할 수 있다.
+
+        if(map.containsKey(key)) { // 키값 있는지 확인, boolean 타입 리턴
+            System.out.println(key);
+            System.out.println(key + "번 게시물을 조회합니다..");
+            System.out.println(map.get(key));
+
+        }
+        else {
+            System.out.println(key + " 번 게시물은 존재하지 않습니다..");
+        }
+
+//        for (key=0; key<map.size(); key++) {
+//
+//            key = Integer.parseInt(tmpSplit[0]);
+//            if(map.containsKey(key)) { // 키값 있는지 확인, boolean 타입 리턴
+//                System.out.println(key);
+//                System.out.println(key + "번 게시물을 조회합니다..");
+//                System.out.println(map.get(key));
+//                break;
+//            }
+//            else {
+//                System.out.println(key + " 번 게시물은 존재하지 않습니다..");
+//                break;
+//            }
+//
+//        }
+
+//        for (Integer key : map.keySet()) { // key값은 primitive 타입은 올수없고, Reference type인 Integer로 정수형을 사용할 수 있다.
+
+//            key = Integer.parseInt(tmpSplit[0]);
+//            if(map.containsKey(key)) { // 키값 있는지 확인, boolean 타입 리턴
+//                System.out.println(key);
+//                System.out.println(key + "번 게시물을 조회합니다..");
+//                System.out.println(map.get(key));
+//                break;
+//            }
+//            else {
+//                System.out.println(key + " 번 게시물은 존재하지 않습니다..");
+//                break;
+//            }
+
+//            if (Integer.parseInt(tmpSplit[0]) == key) {
+//                System.out.println(key + "번 게시물을 조회합니다..");
+//                System.out.println(map.get(key)); // 괜히 arrList써서 key값 찾으려면 다시 split하고 반복해야함..
+//                break;
+//            }
+
     }
 
     // 3. Update 게시글 수정
     public void boardUpdate() {
-        System.out.println(title + "을/를 수정중");
+
+        System.out.print("어떤 게시물을 수정할까요? ");
+        System.out.println("등록된 게시물 번호 : " + map.keySet());
+        System.out.println("입력 예시 : 1번 또는 1 ");
+
+        String[] tmpSplit = inputCmd().split("번");
+
+        Integer key = Integer.parseInt(tmpSplit[0]);
+        // key값은 primitive 타입은 올수없고, Reference type인 Integer로 정수형을 사용할 수 있다.
+
+        if(map.containsKey(key)) { // 키값 있는지 확인, boolean 타입 리턴
+            System.out.println(key);
+            System.out.println(key + "번 게시물을 수정합니다..");
+
+            map.remove(key);
+            ArrayList<String> arrTmp = new ArrayList<String>();
+
+            System.out.print("제목을 입력하세요 > ");
+            String tmpTitle = inputCmd();
+
+            System.out.print("내용을 입력하세요 > ");
+            String tmpContent = inputCmd();
+
+            arrTmp.add(tmpTitle);
+            arrTmp.add(tmpContent);
+
+            map.put(key, arrTmp);
+            System.out.println("맵 밸류 " + map.values());
+            System.out.println("키셋 " + map.keySet());
+
+            System.out.println(map.get(key));
+
+        }
+        else {
+            System.out.println(key + " 번 게시물은 존재하지 않습니다..");
+        }
+
     }
 
     // 4. Delete, 게시글 삭제
     public void boardDelete() {
-        System.out.println(title + "을/를 삭제중.");
+
+        System.out.print("어떤 게시물을 삭제할까요? ");
+        System.out.println("등록된 게시물 번호 : " + map.keySet());
+        System.out.println("입력 예시 : 1번 또는 1 ");
+
+        String[] tmpSplit = inputCmd().split("번");
+
+        Integer key = Integer.parseInt(tmpSplit[0]);
+        // key값은 primitive 타입은 올수없고, Reference type인 Integer로 정수형을 사용할 수 있다.
+
+        if(map.containsKey(key)) { // 키값 있는지 확인, boolean 타입 리턴
+            System.out.println(key);
+            System.out.println(key + "번 게시물을 삭제합니다..");
+
+            map.remove(key);
+            System.out.println(key + "번 게시물이 성공적으로 삭제되었습니다..!");
+            System.out.println(map.get(key));
+
+        }
+        else {
+            System.out.println(key + " 번 게시물은 존재하지 않습니다..");
+        }
+
     }
+
 
     // 5. Exit, 게시글 종료
     public void boardExit() {
@@ -82,7 +221,9 @@ public class Board {
     }
     // 6. Category, 게시글 목록
     public void boardCategory() {
-        System.out.println(title + "목록");
+
+        System.out.println("총 게시글 : " + map.values());
+
     }
 
     // getter, 클래스내 private 멤버변수 접근 가능
@@ -107,16 +248,28 @@ public class Board {
     public void setContent(String content) { // 게시글 내용
         this.content = content;
     }
+
     public static String inputCmd() {
 
         String userCmd;
         Scanner sc = new Scanner(System.in);
         userCmd = sc.nextLine().trim(); // trim() : 앞 뒤 공백 제거
 
-
-        // 잘못된 입력 ,종료 예외처리
         return userCmd;
+//        String strLast = userCmd.substring(userCmd.length()-1);
+//
+//        if(strLast.equals("번")){
+//            return userCmd;
+//        }
+//        // 잘못된 입력 ,종료 예외처리 부분 만들어줘야함
+//
+//        // 사이 공백문자 예외처리 해야함
+//        else {
+//            return("잘못된 입력입니다.");
 
     }
+//    public String toString() {
+//        return String.format("%")
+//    }
 
 }
